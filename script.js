242436d09bc5b7984490380af2ad1e05e94211a2
i-hitesh <hiteshbpandey@gmail.com>
2023-11-11 23:46:13 +0530

    added gsap IN OUT effect on home page

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
+// for smooth scrolling
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
 
+// for cursor movement
 function mouseFollower() {
     window.addEventListener("mousemove", function (dets) {
         document.querySelector("#cursorpoint").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
     })
 }
 
+// for in out Effect through GSAP
+function homepage() {
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+
+        .to(".boundingElement", {
+            y: 0,
+            ease: Expo.easeInOut,
+            duration: 2,
+            stagger: .2,
+        })
+
+}
+
+
+
+
 mouseFollower();
+homepage();
