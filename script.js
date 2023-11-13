ba153895d3e2d1670b71d1107a339544598a2a76
i-hitesh <hiteshbpandey@gmail.com>
2023-11-13 09:57:51 +0530

    fixed mouse follower

--- script.js
+++ script.js
@@ -5,13 +5,49 @@ const scroll = new LocomotiveScroll({
 });
 
 // for cursor movement
-function mouseFollower() {
+// function mouseFollower() {
+//     window.addEventListener("mousemove", function (dets) {
+//         document.querySelector("#cursorpoint").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+//     })
+// }
+
+
+var timeout;
+function mouseShape() {
+
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+
     window.addEventListener("mousemove", function (dets) {
-        document.querySelector("#cursorpoint").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+        clearTimeout(timeout);
+
+        xscale = gsap.utils.clamp(.8, 1.2, dets.clientX - xprev);
+        yscale = gsap.utils.clamp(.8, 1.2, dets.clientY - yprev);
+
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+
+        mouseFollower(xscale, yscale);
+
+        timeout = setTimeout(function () {
+            document.querySelector("#cursorpoint").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)  scale(1, 1)`;
+        }, 100);
+    });
+
+}
+
+
+// for cursor movement
+function mouseFollower(xscale, yscale) {
+    window.addEventListener("mousemove", function (dets) {
+        document.querySelector("#cursorpoint").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)  scale(${xscale}, ${yscale})`;
     })
 }
 
-// for in out Effect through GSAP
+// for in / out Effect through GSAP
 function homepage() {
     var tl = gsap.timeline();
 
@@ -44,8 +80,18 @@ function homepage() {
 
 }
 
+document.querySelectorAll("#element").forEach(function (element) {
+    element.addEventListener("mousemove", function (details) {
+        gsap.to(element.querySelector("img"), {
+            opacity: 1,
+            ease: Power1,
+        });
+    });
+
+});
 
 
 
+mouseShape();
 mouseFollower();
 homepage();
